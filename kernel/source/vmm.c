b243f30ebe7f5a02afbcae0ffe5933fd331e67af
fincs <fincs.alt1@gmail.com>
2014-05-02 17:58:29 +0200

    Implement new kernel mode VMM API

--- kernel/source/vmm.c
+++ kernel/source/vmm.c
@@ -8,9 +8,24 @@ static inline void* _CleanMapAddr(void* addr)
 	return (void*)((u32)addr &~ 0xFFF);
 }
 
+static inline size_t _SizeToPageCount(size_t size)
+{
+	return (int)((size+0xFFF) >> 12);
+}
+
 static inline bool _IsKernel(void* addr)
 {
 	return (u32)addr >= 0x80000000;
+}
+
+static inline u32 _GetPermissions(int flags, bool isK)
+{
+	static u32 flagCnv[8] =
+	{
+		MMU_L2_RORO | MMU_L2_XN_4K, MMU_L2_RWRW | MMU_L2_XN_4K, MMU_L2_RORO, MMU_L2_RWRW,
+		MMU_L2_RONA | MMU_L2_XN_4K, MMU_L2_RWNA | MMU_L2_XN_4K, MMU_L2_RONA, MMU_L2_RWNA,
+	};
+	return flagCnv[(flags & 3) + (isK ? 4 : 0)];
 }
 
 static inline vu32* _GetVMTable(void* addr, semaphore_t** ppSem, processInfo* ps)
@@ -137,149 +152,159 @@ vu32* VmmGetEntry(vmminfo_t* vmInfo, void* addr, bool bCreate)
 	return vmInfo->l2_table + ((idx & 1) << 9) + L2_INDEX(addr);
 }
 
-static inline u32 _GetPermissions(int flags, bool isK)
-{
-	static u32 flagCnv[8] =
-	{
-		MMU_L2_RORO | MMU_L2_XN_4K, MMU_L2_RWRW | MMU_L2_XN_4K, MMU_L2_RORO, MMU_L2_RWRW,
-		MMU_L2_RONA | MMU_L2_XN_4K, MMU_L2_RWNA | MMU_L2_XN_4K, MMU_L2_RONA, MMU_L2_RWNA,
-	};
-	return flagCnv[(flags & 3) + (isK ? 4 : 0)];
-}
+//-------------------------------------------------------------------------------------------------
 
-void* MemMapPage(void* vaddr, int flags)
+#define INCR_ADDR(_x,_k) ((void*)((word_t)(_x) + (_k)))
+
+size_t MemVmMap(processInfo* proc, void* addr, size_t size, int flags, pageinfo_t* pages[])
 {
-	vaddr = _CleanMapAddr(vaddr);
+	addr = _CleanMapAddr(addr);
+	size = _SizeToPageCount(size);
+	if (!size) return 0; // bail out
+
 	vmminfo_t vmInfo;
-	if (!VmmOpen(&vmInfo, nullptr, vaddr))
-		return nullptr; // bail out
+	if (!VmmOpen(&vmInfo, nullptr, addr))
+		return 0; // bail out
 
-	vu32* entry = VmmGetEntry(&vmInfo, vaddr, true);
-	if (!entry)
+	size_t i;
+	for (i = 0; i < size; i ++, addr = INCR_ADDR(addr, 0x1000))
 	{
-		VmmClose(&vmInfo);
-		return nullptr; // bail out
+		pageinfo_t* page = nullptr;
+		if (pages)
+		{
+			page = *pages++;
+			if (!page)
+				break; // Invalid page handle
+			if (page->flags & PAGEFLAG_HASCOLOUR && (page->flags & PAGEFLAG_COLOURMASK) != PAGE_COLOUR(addr))
+				break; // Page colouring restriction violated
+		}
+
+		vu32* entry = VmmGetEntry(&vmInfo, addr, true);
+		if (!entry)
+			break; // Cannot retrieve entry pointer
+
+		if (*entry & 3)
+			break; // Page already occupied
+
+		if (!page)
+		{
+			page = MemAllocPage(PAGEORDER_4K);
+			if (!page)
+				break; // Out of memory
+		}
+
+		// Initialize page structure
+		page->flags |= PAGEFLAG_HASCOLOUR | PAGE_COLOUR(addr);
+		VmmPageIncrRef(&vmInfo);
+
+		// Zerofill page memory
+		u32* pageMem = (u32*)page2vphys(page);
+		int i;
+		for (i = 0; i < 1024; i ++)
+			pageMem[i] = 0;
+
+		bool isK = _IsKernel(addr);
+		u32 setting = MMU_L2_PAGE4K | MMU_L2_4K_CACHED | (u32)virt2phys(pageMem) | _GetPermissions(flags, isK);
+		if (!isK)
+			setting |= MMU_L2_NG; // non-global
+
+		*entry = setting;
+		// No need to do anything further:
+		// - TLB doesn't store entries that do not exist
+		// - Cache doesn't cache invalid addresses
 	}
-
-	bool isK = _IsKernel(vaddr);
-
-	if (*entry & 3)
-	{
-		VmmClose(&vmInfo);
-		return nullptr; // Page already exists
-	}
-
-	// Allocate a new page
-	pageinfo_t* pPage = MemAllocPage(PAGEORDER_4K);
-	if (!pPage)
-	{
-		VmmClose(&vmInfo);
-		return nullptr; // Out of memory
-	}
-
-	// Initialize page structure
-	pPage->flags |= PAGEFLAG_HASCOLOUR | PAGE_COLOUR(vaddr);
-	VmmPageIncrRef(&vmInfo);
-
-	// Zerofill page memory
-	u32* pageMem = (u32*)page2vphys(pPage);
-	int i;
-	for (i = 0; i < 1024; i ++)
-		pageMem[i] = 0;
-
-	u32 setting = MMU_L2_PAGE4K | MMU_L2_4K_CACHED | (u32)virt2phys(pageMem) | _GetPermissions(flags, isK);
-	if (!isK)
-		setting |= MMU_L2_NG; // non-global
-
-	*entry = setting;
-	// No need to do anything further:
-	// - TLB doesn't store entries that do not exist
-	// - Cache doesn't cache invalid addresses
-	VmmClose(&vmInfo);
-	return vaddr;
-}
-
-bool MemProtectPage(void* vaddr, int flags)
-{
-	vaddr = _CleanMapAddr(vaddr);
-	vmminfo_t vmInfo;
-	if (!VmmOpen(&vmInfo, nullptr, vaddr))
-		return nullptr; // bail out
-
-	vu32* entry = VmmGetEntry(&vmInfo, vaddr, false);
-	if (!entry)
-	{
-		VmmClose(&vmInfo);
-		return nullptr; // bail out
-	}
-
-	bool isK = _IsKernel(vaddr);
-
-	if (!(*entry & 2))
-	{
-		VmmClose(&vmInfo);
-		return false; // Missing or unsupported page
-	}
-
-	// Change the protection settings
-	u32 setting = *entry;
-	setting &= ~MMU_L2_4K_PMASK;
-	setting |= _GetPermissions(flags, isK);
-	*entry = setting;
-	CpuFlushTlbByAddr(vaddr);
 
 	VmmClose(&vmInfo);
-	return true;
+	return i*0x1000;
 }
 
-bool MemUnmapPage(void* vaddr)
+size_t MemVmProtect(processInfo* proc, void* addr, size_t size, int flags)
 {
-	vaddr = _CleanMapAddr(vaddr);
-#ifdef DEBUG
-	kprintf("<MemUnmapPage> %p to be freed\n", vaddr);
-#endif
+	addr = _CleanMapAddr(addr);
+	size = _SizeToPageCount(size);
+	if (!size) return 0; // bail out
+
 	vmminfo_t vmInfo;
-	if (!VmmOpen(&vmInfo, nullptr, vaddr))
-		return nullptr; // bail out
+	if (!VmmOpen(&vmInfo, nullptr, addr))
+		return 0; // bail out
 
-	vu32* entry = VmmGetEntry(&vmInfo, vaddr, false);
-	if (!entry)
+	size_t i;
+	int asid = proc ? proc->asid : 0;
+	for (i = 0; i < size; i ++, addr = INCR_ADDR(addr, 0x1000))
 	{
-		VmmClose(&vmInfo);
-		return nullptr; // bail out
+		vu32* entry = VmmGetEntry(&vmInfo, addr, false);
+		if (!entry)
+			break; // Cannot retrieve entry pointer
+
+		if (!(*entry & 2))
+			break; // Missing or unsupported page
+
+		// Change the protection settings
+		u32 setting = *entry;
+		setting &= ~MMU_L2_4K_PMASK;
+		setting |= _GetPermissions(flags, _IsKernel(addr));
+		*entry = setting;
+		CpuFlushTlbByAddrAndASID(addr, asid);
 	}
 
-	if (!(*entry & 2))
-	{
-		VmmClose(&vmInfo);
-		return false; // Missing or unsupported page
-	}
+	VmmClose(&vmInfo);
+	return i*0x1000;
+}
 
-	// Retrieve page structure
-	pageinfo_t* pPage = vphys2page(phys2virt(*entry & ~0xFFF));
+size_t MemVmUnmap(processInfo* proc, void* addr, size_t size)
+{
+	addr = _CleanMapAddr(addr);
+	size = _SizeToPageCount(size);
+	if (!size) return 0; // bail out
 
-	// Free page
-	if (!MemPageDecrRef(pPage))
+	vmminfo_t vmInfo;
+	if (!VmmOpen(&vmInfo, nullptr, addr))
+		return 0; // bail out
+
+	size_t i;
+	int asid = proc ? proc->asid : 0;
+	for (i = 0; i < size; i ++, addr = INCR_ADDR(addr, 0x1000))
 	{
+		vu32* entry = VmmGetEntry(&vmInfo, addr, false);
+		if (!entry)
+			break; // Cannot retrieve entry pointer
+
+		if (!(*entry & 2))
+			break; // Missing or unsupported page
+
+		// Retrieve page structure
+		pageinfo_t* pPage = vphys2page(phys2virt(*entry & ~0xFFF));
+
+		// Free page
+		if (!MemPageDecrRef(pPage))
+		{
 #ifndef HAS_PIPT_CACHE
-		// Flush page from the caches
-		DC_FlushRange(vaddr, 0x1000);
-		IC_InvalidateRange(vaddr, 0x1000);
+			if (!proc || _IsKernel(addr)) // i.e. current process or kernel address
+			{
+				// Flush page from the caches
+				DC_FlushRange(addr, 0x1000);
+				IC_InvalidateRange(addr, 0x1000);
+			} else
+			{
+				// I'm sorry, but this has to be done the hard way...
+				DC_FlushAll();
+				IC_InvalidateAll();
+			}
 #endif
-		// Delete page
-		if (!(pPage->flags & PAGEFLAG_STICKY))
-			MemFreePage(pPage, PAGEORDER_4K);
+			// Delete page
+			if (!(pPage->flags & PAGEFLAG_STICKY))
+				MemFreePage(pPage, PAGEORDER_4K);
+		}
+
+		// Zap entry and clear TLB
+		*entry = 0;
+		entry[256] = 0;
+		VmmPageDecrRef(&vmInfo);
+		CpuFlushTlbByAddrAndASID(addr, asid);
 	}
 
-	// Zap entry and clear TLB
-	*entry = 0;
-	entry[256] = 0;
-	VmmPageDecrRef(&vmInfo);
-	CpuFlushTlbByAddr(vaddr);
-
-	// Return - this will also take care of deleting the L2 table if necessary
 	VmmClose(&vmInfo);
-	return true;
+	return i*0x1000;
 }
 
 u32 MemTranslateAddr(void* address)
--- sdk/include/feos/kernel/vmm.h
+++ sdk/include/feos/kernel/vmm.h
@@ -3,12 +3,64 @@
 #error "You must include <feos.h> first!"
 #endif
 
-// Allocates a new page and maps it to the specified address.
-void* MemMapPage(void* address, int flags);
-// Sets the permissions of a page.
-bool MemProtectPage(void* address, int flags);
-// Frees a page previously mapped by MemMapPage.
-bool MemUnmapPage(void* address);
+// Maps pages into the VM address space of a process at the specified address.
+// proc  - The handle of the process to manipulate. If NULL, the current process is manipulated.
+//         This handle is ignored when the address ís a kernel address, in which case the kernel
+//         VM address space is manipulated instead.
+// addr  - The address at which to (sequentially) map the pages; which is forcefully aligned down
+//         to a page boundary.
+// size  - The size of the memory region to map (in bytes); which is forcefully rounded up to
+//         a multiple of the page size.
+// flags - The access permissions (a combination of PAGE_R, PAGE_W and PAGE_X).
+//         Kernel-space addresses are never readable by user mode.
+// pages - An array of handles to pages to map. If NULL, new pages are allocated.
+//         If the array is specified, there *MUST* be enough pages in it to cover the whole
+//         specified memory region, otherwise the function exhibits undefined behaviour.
+//         The kernel takes ownership of all pages that are successfully mapped.
+// Returns:
+//   The number of successfully mapped pages times the page size until the first error occured.
+size_t MemVmMap(processInfo* proc, void* addr, size_t size, int flags, pageinfo_t* pages[]);
+
+// Changes the access permissions for a memory region inside the VM address space of a process.
+// proc  - The handle of the process to manipulate. If NULL, the current process is manipulated.
+//         This handle is ignored when the address ís a kernel address, in which case the kernel
+//         VM address space is manipulated instead.
+// addr  - The start address of the memory region whose access permissions to change; which is
+//         forcefully aligned down to a page boundary.
+// size  - The size of the memory region whose access permissions to change (in bytes); which is
+//         forcefully rounded up to a multiple of the page size.
+// flags - The access permissions to apply (a combination of PAGE_R, PAGE_W and PAGE_X).
+//         Kernel-space addresses are never readable by user mode.
+// Returns:
+//   The number of successfully re-protected pages times the page size until the first error occured.
+size_t MemVmProtect(processInfo* proc, void* addr, size_t size, int flags);
+
+// Unmaps a memory region from the VM address space of a process.
+// proc  - The handle of the process to manipulate. If NULL, the current process is manipulated.
+//         This handle is ignored when the address ís a kernel address, in which case the kernel
+//         VM address space is manipulated instead.
+// addr  - The start address of the memory region to unmap; which is forcefully aligned down to
+//         a page boundary.
+// size  - The size of the memory region to unmap (in bytes); which is forcefully rounded up to
+//         a multiple of the page size.
+// Returns:
+//   The number of successfully unmapped pages times the page size until the first error occured.
+size_t MemVmUnmap(processInfo* proc, void* addr, size_t size);
 
 // Translates a virtual address to a physical address. ~0 = failure
 u32 MemTranslateAddr(void* address);
+
+static inline void* MemMapPage(void* vaddr, int flags)
+{
+	return MemVmMap(nullptr, vaddr, 0x1000, flags, nullptr) ? vaddr : nullptr;
+}
+
+static inline bool MemProtectPage(void* vaddr, int flags)
+{
+	return MemVmProtect(nullptr, vaddr, 0x1000, flags) != 0;
+}
+
+static inline bool MemUnmapPage(void* vaddr)
+{
+	return MemVmUnmap(nullptr, vaddr, 0x1000) != 0;
+}
